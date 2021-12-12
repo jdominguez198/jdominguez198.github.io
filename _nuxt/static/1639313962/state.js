281ed5d58e4a9755cf88a0605a79cd52dc482856
jdominguez198 <jdominguez198@users.noreply.github.com>
2021-12-12 13:05:00 +0000

    deploy: 46fccba2095cdb806168e1f763e9deb343a22539

--- _nuxt/static/1639313962/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639313962",layout:"default",error:a,serverRendered:true,routePath:b,config:{contactFormBackendUrl:"https:\u002F\u002Fformspree.io\u002Ff\u002Fxbjqbove",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null,"\u002F"));
